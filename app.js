29e4b58f3f8fd2d6468fafe93bf9eaef01e785dd
lumijoe <spacedesign63@gmail.com>
2023-11-07 10:49:01 +0900

    Update the text in app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// list2-1(p56)1106
+// list2-1(p56)1106,1107
 // app.js
 
-// httpを用意
+// モジュールがロードされて変数（定数）にオブジェクトとして設定=httpというモジュール名を用意
 const http = require('http');
-// サーバーを用意
+// createServerメソッドでサーバーオブジェクトを作成
 var server = http.createServer(
-    (request,response) => {
+    // c要求→s, s返信→c アクセスされると実行される処理
+    (request, response) => {
+        // 返信する.終了する(テキスト部分を表示)
         response.end('Hello Node.js!');
     }
 );
 // 待ち受け開始
 server.listen(3000);
+
