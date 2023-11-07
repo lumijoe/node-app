7989b01c17df7655953c98fe58034b8abc0ac57a
lumijoe <spacedesign63@gmail.com>
2023-11-07 11:40:50 +0900

    page67-69

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-// list2-1(p56)1106,1107
+// list2-2(p67)1107
 // app.js
 
-// モジュールがロードされて変数（定数）にオブジェクトとして設定=httpというモジュール名を用意
+/* 
+1: モジュールがロードされて変数（定数）にオブジェクトとして設定=httpというモジュール名を用意
+2: createServerメソッドでサーバーオブジェクトを作成
+    -c要求→s, s返信→c アクセスされると実行される処理
+    -返信する.終了する(テキスト部分を表示)
+3: 待ち受け開始
+*/
+
 const http = require('http');
-// createServerメソッドでサーバーオブジェクトを作成
+
 var server = http.createServer(
-    // c要求→s, s返信→c アクセスされると実行される処理
     (request, response) => {
-        // 返信する.終了する(テキスト部分を表示)
-        response.end('Hello Node.js!');
+        response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
     }
 );
-// 待ち受け開始
-server.listen(3000);
 
+server.listen(3000);
