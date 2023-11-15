91debb342bb441870d074ead545adba78b9a6c7f
lumijoe <spacedesign63@gmail.com>
2023-11-15 16:51:53 +0900

    page138

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// list3-11(p134)1115
+// list3-14(p137)1115
 // app.js
 
 /* 
@@ -78,13 +78,49 @@ var data2 = {
 };  
 
 // ★indexのアクセス処理
+
+var data = { msg: 'no message...' };
+
 function response_index(request, response) {
+    // POSTアクセス時の処理
+    if (request.method == 'POST') {
+        var body = '';
+
+        // データ受信のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        // データ受信終了のイベント処理
+        request.on('end', () => {
+            data = qs.parse(body);  // ★データのパース
+            write_index(request, response);
+        });
+    } else {
+        write_index(request, response);
+    }
+}
+/* list3−14（p137）1115実装でコメントアウト
     var msg = "これはIndexページです。"
     var content = ejs.render(index_page, {
         title: "Index",
         content: msg,
         data: data,
         filename: 'data_item' // ★追記
+    });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(content);
+    response.end();
+}
+*/
+
+// indexの表示の作成
+function write_index(request, response) {
+    var msg = "※伝言を表示します。"
+    var content = ejs.render(index_page, {
+        title: "Index",
+        content: msg,
+        data: data,
     });
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(content);
